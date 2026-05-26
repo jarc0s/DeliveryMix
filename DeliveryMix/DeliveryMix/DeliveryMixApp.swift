ab2f74c99788b411fffe3ec111b222970e2fcc16
jarcos <juan.arcos.hdz@gmail.com>
2026-05-25 18:22:47 -0600

    Initial Project

---
+++ DeliveryMix/DeliveryMix/DeliveryMixApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeliveryMixApp.swift
+//  DeliveryMix
+//
+//  Created by Jarkos on 25/05/26.
+//
+
+import SwiftUI
+
+@main
+struct DeliveryMixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
